f5bf762530a7da9804a6ab9a6371ff19e7339a1a
Jordan Hudgens <jordan@wow.com>
2015-12-26 12:18:42 -0600

    Updated content and tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,10 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def new
@@ -13,15 +12,9 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @student[:first_name] = params[:first_name]
-    @student[:last_name] = params[:last_name]
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
     @student.save
     redirect_to student_path(@student)
   end
-
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
