0cfb22ffe8d8c478e33f8a450f30e0a1c4fd7511
Charles Pletcher <cpletcher2@gmail.com>
2016-04-25 11:40:50 -0400

    Actually solve the lab according to instructions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,10 +11,12 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
-    @student.save
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
     redirect_to student_path(@student)
   end
 end
