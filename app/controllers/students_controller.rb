d344e3d889ac289deb9c33d5d5f373c6c8a5b2f8
Charles Pletcher <cpletcher2@gmail.com>
2016-04-25 11:40:50 -0400

    Actually solve the lab according to instructions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,27 +1,22 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @student = Student.new
-    @student[:first_name] = params[:first_name]
-    @student[:last_name] = params[:last_name]
-    @student.save
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
     redirect_to student_path(@student)
   end
-
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
