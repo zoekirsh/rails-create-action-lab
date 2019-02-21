b983741660682f08c447a02f6aa6ee453d466e56
maxwellbenton <maxwellbenton@gmail.com>
2019-02-21 15:52:46 -0500

    updates deprecated controller test

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -6,12 +6,12 @@ describe StudentsController do
     context "with valid attributes" do
       it "creates a new student" do
         expect{
-          post :create, { :first_name => "Sam", :last_name => "Smith" }
+          post :create, params: { :first_name => "Sam", :last_name => "Smith" }
         }.to change(Student,:count).by(1)
       end
 
       it "redirects to the new student" do
-        post :create, { :first_name => "Sam", :last_name => "Smith" }
+        post :create, params: { :first_name => "Sam", :last_name => "Smith" }
         expect(response).to redirect_to Student.last
       end
     end
